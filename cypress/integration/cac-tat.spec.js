0f14dd04e7838a0617b42c361365e68b42895074
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2022-09-26 21:46:33 -0300

    feat: criando testes para selecionar arquivo com drag and drop

--- cypress/integration/cac-tat.spec.js
+++ cypress/integration/cac-tat.spec.js
@@ -13,4 +13,15 @@ describe("CAT TAT Testes", () => {
         expect(input[0].files[0].name).to.equal("example.json");
       });
   });
+
+  it("seleciona um arquivo simulando um drag-and-drop", () => {
+    cy.get("#file-upload")
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json", {
+        action: "drag-drop",
+      })
+      .should((input) => {
+        expect(input[0].files[0].name).to.equal("example.json");
+      });
+  });
 });
