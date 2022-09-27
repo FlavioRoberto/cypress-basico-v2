161d922a6a16e3eb9adbd5f6a0c693d17b767fe7
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2022-09-26 21:42:44 -0300

    feat: criando testes para selecionar arquivo

---
+++ cypress/integration/cac-tat.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe("CAT TAT Testes", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+  it("seleciona um arquivo da pasta fixture", () => {
+    cy.get("#file-upload")
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json")
+      .should((input) => {
+        expect(input[0].files[0].name).to.equal("example.json");
+      });
+  });
+});
